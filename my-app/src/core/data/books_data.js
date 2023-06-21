6a134c203d695284f95dd0634adc1ebde3819b6c
Victor <victornpng@outlook.com>
2023-06-21 17:19:20 -0300

    fix images

--- my-app/src/core/data/books_data.js
+++ my-app/src/core/data/books_data.js
@@ -1,13 +1,26 @@
-const booksData = [{
-    title: 'I Will Teach You to Be Rich',
-    author: 'Ramit Sethi',
-    img: "./assets/images/book-one.jpg",
-    id: 1,
-}, {
-    title: 'Greenlights ',
-    author: 'Matthew McConaughey',
-    img: "https://images-na.ssl-images-amazon.com/images/I/61-Pt+1b+xL._AC_UL600_SR600,400_.jpg",
-    id: 2,
-},];
+import img1 from '../../assets/images/book-one.jpg';
+import img2 from '../../assets/images/book-two.jpg';
+import img3 from '../../assets/images/book-three.jpg';
+
+const booksData = [
+    {
+        title: 'I Will Teach You to Be Rich',
+        author: 'Ramit Sethi',
+        img: img1,
+        id: 1,
+    },
+    {
+        title: 'Greenlights',
+        author: 'Matthew McConaughey',
+        img: img2,
+        id: 2,
+    },
+    {
+        title: 'Fourth Wing',
+        author: 'Rebecca Yarros',
+        img: img3,
+        id: 3,
+    },
+];
 
 export default booksData;
